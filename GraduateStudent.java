42b3d40b07dd41487680fc4020da321b6d715ad5
amjadali448 <66878467+amjadali448@users.noreply.github.com>
2021-04-28 01:31:59 +0500

    Add files via upload

---
+++ GraduateStudent.java
@@ -0,0 +1,6 @@
+public class GraduateStudent {
+	 public void exam()
+	    {
+	        System.out.println("The graduate student gives a written paper");
+	    }
+}
---
+++ PhDStudent.java
@@ -0,0 +1,6 @@
+public class PhdStudent extends Student {
+	 public void exam()
+	    {
+	        System.out.println("Phd Student takes exam by giving his final defense presentation");
+	    }
+}
